32449f6a14779d6c23e4a347fa6ca30892a9cefc
MarcoPerta <perta.marco@gmail.com>
2022-10-24 17:13:16 +0200

    bug fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ btn.addEventListener("click", GetAnswer);
 // variabile email
 let email = document.getElementById('InputEmail');
 // variabile output email
-let outEmail = document.getElementById('OutEmail');
+let outEmail = document.getElementById('outEmail');
 
 // Definisco il gruppo di mail accettate
 let members = ["pertamarco@mail.com","lucalioni@mail.com","marcellobo@mail.com","paolono@mail.com","andreasi@mail.com"]
@@ -14,7 +14,7 @@ function GetAnswer(){
   let emailValue = email.value
   console.log(emailValue)
 
-  for(let i=0; i> members.length ; i++){
+  for(let i=0; i < members.length ; i++){
     if (emailValue===members[i]){
         console.log('la mail è presente nella lista')
         outEmail.innerHTML = `la mail : ${emailValue} può accedere` 
